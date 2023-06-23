27263b9da11df4ebe9b94ab38a0693a0fb33373d
Cedric Soares <cedricsoares@me.com>
2023-06-23 19:18:21 +0200

    Add get_news_sections() function

--- src/etl/etl.py
+++ src/etl/etl.py
@@ -3,21 +3,22 @@ import json
 import logging
 import os
 import time
-from typing import Dict, List, Optional, Any
+from typing import Dict, List, Tuple, Optional, Any
 
 import requests
 from constants import (API_CALL_DAILY_INDEX, BOOKS_MAPPING, END_POINT_HITS,
                        INDEXES_SETTINGS, INDEXEXES_NAMES, NEWSWIRE_MAPPING,
                        OFFSET_FACTOR)
+from elasticsearch import Elasticsearch
+from utils.get_information_helpers import (build_query, results_to_list, 
+                                           bulk_to_elasticsearch)
 
 from dotenv import load_dotenv
-from elasticsearch import Elasticsearch
-from elastic_transport import ObjectApiResponse
-from elasticsearch.helpers import bulk
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+API_CALLS = 0
 
 
 def get_elactic_connection():
@@ -53,87 +54,35 @@ def create_index(con: Elasticsearch, name: str,
         logging.warning(f'----- Failed to create {name} index. -----')
 
 
-def build_query(index_name: str, api_key: str, 
-                start_offset: Optional[int], news_section: Optional[str],
-                movies_type: Optional[str]) -> str:
-    """ Build query to pass to the NYT API
-        
-        Query is built according to type of content we try to get data
+def get_news_sections(con: Elasticsearch, api_key: str, 
+                          api_calls: int) -> Tuple(List(str), int):
+    """ Get list of news section from NYT API
 
     Args:
-        index_name (str): Specify type of the content we want to retrieve
-            via NYT API. Must be news, books, movies
-        start_offset (int): Specify the offset number to start retriving data.
-            Only used for books and movies
-        news_section: Name of the news section.
-            Only used for news.
-        movies_type: Name of type of movies.
-            Only used on movirs.
-
-    Return:
-        str: Builded querry regarding passed parameters
-    """
-    logging.info('----- Strat building querry for NYT API -----')
-
-    if index_name == 'news':
-        query = 'https://api.nytimes.com/svc/news/v3/content/all/{news_section}.json?&api-key={api_key}'
-        logging.info(f'----- Builded querry {query} -----')
-        return query
-
-    if index_name == 'books':
-        query = f'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?offset={start_offset}&api-key={api_key}'
-        logging.info(f'----- Builded querry {query} -----')
-        return query
-
-    if index_name == 'movies':
-        query = f'https://api.nytimes.com/svc/movies/v2/reviews/all.json?offset={start_offset}&api-key={api_key}'
-        logging.info(f'----- Builded querry {query} -----')
-        return query
-
-
-def results_to_list(index_name: str,
-                    results: List[Dict[str, Any]]) -> List[Dict[str, Dict[Any]]]:
-    """Transform a list of documents from NTY API to dict to bulk on Elasticsearch
-  
-    Args:
-        index_name (str): index_name to provide to bulk data to Elasticsearch
-        results (list): A list of documents retrived by NYT API
-
-    Retuns
-        bulk_list (lits): A list of index_name / documents ready to bulk on Elasticsearch
-    """
-    logging.info(f'----- Start building bulk list for {index_name} index -----')
-    actions = []
-    for doc in results:
-        action = {
-            "_index": index_name,
-            "_source": doc
-        }
-        actions.append(action)
-    logging.info(f'----- List to bulk on Elasticsearch : \n {actions} \n -----')
-    return actions
-
-
-def bulk_to_elasticsearch(
-        con: Elasticsearch, bulk_list: Dict[str, Dict[Any]]
-        ) -> ObjectApiResponse[Any][Any]:
-    """ Run Elasticsearch Bulk API with results from NYT API 
-    
-    Args:
-        con (Elasticsearch): 
-        bult_list (list): A list of documents with index_names from NYT API results
+        con (Elasticsearch): Connector object used to connect to database
+        api_key (str): Used NYT api key to retrieve data
+        api_call (int): Number of API calls
 
     Returns:
-        ObjectApiREsponse : Response from Elasticsearh Bulk API call
+        sections (list): List of news sections retrieved from NYT API
+        api_calls (int): number of calls done by the script
     """
-    logging.info('----- Start calling Elasticsearck Bulk API -----')
-    bulk(con, bulk_list)
-    
+    logging.info('----- Retrieving news sections -----')
+    query = build_query(index_name='news_sections', api_key=api_key)
+    results = requests.get(query)
+    sections = [item['section'] for item in results.json()['results']]
+    logging.info(f'----- News sections: \n {sections} \n')
 
-def get_books_or_movies(con: Elasticsearch, index_name: str,
-                start_offset: int, results_by_page: int,
+    api_calls += 1
+
+    return sections, api_calls
+
+
+def get_books_or_movies_data(con: Elasticsearch, index_name: str,
+                start_offset: int, results_by_page: int, api_cals: int,
                 max_api_calls: int, api_key: str) -> int:
-    """Get documents from books API
+    """Get books or movies documents from books API
+        For both logic is the same due to API calls limites
 
     Args:
         con (Elasticsearch): Connector object used to connect to database
@@ -142,17 +91,15 @@ def get_books_or_movies(con: Elasticsearch, index_name: str,
         enpoint_hits (int): Number of returned hits from NYT API books
         start_offset (int): Offset number to pass to the API call in
             order to specify where to start retrieving data
+        api_calls (int): Number of API calls since start of the script 
         results_by_page (int): Number of results of each reponse from NYT API calls
         max_api_calls (int): Maximum of dailly calls allowed by the NYT API
         api_key (str): Used NYT api key to retrieve data
 
     Returns:
-        api_call (int): number api calls used by the function
+        api_calls (int): number api calls used by the function
     """
-
     logging.info(f'----- Start getting {index_name} from NYT API -----')
-
-    api_calls = 1
 
     while (api_calls < max_api_calls):
 
@@ -172,7 +119,7 @@ def get_books_or_movies(con: Elasticsearch, index_name: str,
         logging.info(f"----- Json response page regarding start_offset: {start_offset} \n {res} -----")
 
         docs = res['results']
-        actions = results_by_page(index_name=index_name, results=docs)
+        actions = results_to_list(index_name=index_name, results=docs)
 
         # Perform the bulk indexing
         response = bulk_to_elasticsearch(con, actions)
@@ -194,14 +141,7 @@ def get_books_or_movies(con: Elasticsearch, index_name: str,
         ######################################################
 
         logging.info(f'----- Next offset to use on API call: {start_offset} -----')
-
-
-
-    
-
-    
-
-    
-
+        
+        return api_cals
 
 
