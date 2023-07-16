e8f73b40100235bb642642832c433295a59fa382
ep2112 <ep2112@gmail.com>
2023-07-16 15:21:02 +0200

    file structure

---
+++ dash/main.py
@@ -0,0 +1,9 @@
+import dash
+from dash import dcc,html,dash_table
+import pandas as pd
+import plotly.express as px 
+from dash.dependencies import Output,Input
+import dash_bootstrap_components as dbc
+import queries 
+
+
