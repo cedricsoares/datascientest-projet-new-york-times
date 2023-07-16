642e63a7d2d143679ac4e78188d25bd074e5d086
ep2112 <ep2112@gmail.com>
2023-07-16 16:50:13 +0200

    update requirements

--- dash/main.py
+++ dash/main.py
@@ -23,6 +23,32 @@ import transform
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)
 
+app.layout = html.Div(style={'background-color': 'darkgrey', 'color': 'lightblue'}, children=[
+    html.H1("Welcome to the App"),
+    html.H2("Creators: Cedric Soares, Anna Temerko, Matthieu Lefebrve, and Edouard Philippe"),
+    html.Button("News", id='btn-news', n_clicks=0),
+    html.Button("Books", id='btn-books', n_clicks=0),
+    html.Button("Movies", id='btn-movies', n_clicks=0)
+])
+
+@app.callback(
+    dash.dependencies.Output('url', 'pathname'),
+    [dash.dependencies.Input('btn-news', 'n_clicks'),
+     dash.dependencies.Input('btn-books', 'n_clicks'),
+     dash.dependencies.Input('btn-movies', 'n_clicks')])
+def navigate_to_page(news_clicks, books_clicks, movies_clicks):
+    if news_clicks > 0:
+        return '/news'
+    elif books_clicks > 0:
+        return '/books'
+    elif movies_clicks > 0:
+        return '/movies'
+    else:
+        return '/'
+
+
+
+
 
 ###########################################
 # News ####################################
@@ -35,6 +61,31 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets,suppress_cal
 ###########################################
 # Movies ##################################
 ###########################################
+
+
+###########################################
+# app methods #############################
+###########################################
+
+
+app.validation_layout = html.Div([
+    html.Div([dcc.Location(id='url', refresh=False)]),
+    html.Div(id='page-content')
+])
+
+@app.callback(
+    dash.dependencies.Output('page-content', 'children'),
+    [dash.dependencies.Input('url', 'pathname')]
+)
+def display_page(pathname):
+    if pathname == '/news':
+        return news_page
+    elif pathname == '/books':
+        return books_page
+    elif pathname == '/movies':
+        return movies_page
+    else:
+        return app.layout
 
 
 
